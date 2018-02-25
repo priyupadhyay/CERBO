1247eb0b1fff511c481734337a3d20ffc6539d17
0104ishu <ishu.gunani2@gmail.com>
2018-02-25 07:40:04 +0530

    ytrewq

--- APIs/cerboMid/routes/dataFetch.js
+++ APIs/cerboMid/routes/dataFetch.js
@@ -1,21 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
-// function pyCall(para){
-//     return new Promise(function (sucess, nosuccess) {
+function pyCall(para){
+    return new Promise(function (sucess, nosuccess) {
 
-//     const { spawn } = require('child_process');
-//     const pyprog = spawn('python', ['./../cerboBack/try.py',para]);
+    const { spawn } = require('child_process');
+    const pyprog = spawn('python', ['./../cerboBack/try.py',para]);
     
-//     pyprog.stdout.on('data', function (data) {
-//         sucess(data);
-//     });
-//     pyprog.stderr.on('data', (data) => {
-//         nosuccess(data);
-//         });
-//     });
-// }
+    pyprog.stdout.on('data', function (data) {
+        sucess(data);
+    });
+    pyprog.stderr.on('data', (data) => {
+        nosuccess(data);
+        });
+    });
+}
+router.get('/test', (req, res, next) =>{
 
+    return 'bleh';
+});
 
 router.post('/', (req, res, next) => {
     
@@ -55,36 +58,39 @@ router.post('/', (req, res, next) => {
     return resoomerCall();
 });
 
+router.get('/bleh', (req, res, next) =>{
+   // function resoomerCall(){
 
-function resoomerCall(){
 
-
-    var request = require('request');
-
-    // Set the headers
-    var headers = {
-        'Content-Type':'form-data'
-    }
-
-    // Configure the request
-    var options = {
-        url: 'https://resoomer.pro',
-        method: 'POST',
-        headers: headers,
-        qs: {'API_KEY': '0372FE894651476C945D8EEF3F0DAC36', 'text': "According to nodejs.org, Node.js is a platform built on Chrome's JavaScript runtime for easily building fast, scalable network applications. Node.js uses an event-driven, non-blocking I/O model that makes it lightweight and efficient, perfect for data-intensive real-time applications that run across distributed devices.",'size':'10'}
-    }
-
-    // Start the request
-    request(options, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            console.log(body)
+        var request = require('request');
+        var FormData= require('form-data');
+        
+        // Set the headers
+        var headers = {
+            'Content-Type':'form-data'
         }
-        return body;
-    })
+    
+        // Configure the request
+        var options = {
+            url: 'resoomer.pro/summarizer/',
+            method: 'POST',
+            headers: headers,
+            body: {'API_KEY': '0372FE894651476C945D8EEF3F0DAC36', 'text': "According to nodejs.org, Node.js is a platform built on Chrome's JavaScript runtime for easily building fast, scalable network applications. Node.js uses an event-driven, non-blocking I/O model that makes it lightweight and efficient, perfect for data-intensive real-time applications that run across distributed devices.",'size':'10'}
+        }
+    
+        // Start the request
+        request(options, function (error, response, body) {
+            if (!error && response.statusCode == 200) {
 
-
-
-}
+            }
+            var form=new FormData();
+            form.append('my_data','blleh');
+            return form;
+        })
+        
+   // }
+    
+});
 
 module.exports = router;
 
